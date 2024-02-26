e06c7ab84fee734e6e1b614655a414a35781a836
gdstergiopoulos <gdstergiopoulos@gmail.com>
2024-02-26 23:01:16 +0200

    get cookies as str

--- lab1.py
+++ lab1.py
@@ -16,15 +16,18 @@ url = input("Insert a valid URL: ")  # προσδιορισμός του url
 with requests.get(url) as response:  # το αντικείμενο response
     # html = response.text
     # more(html)
+
+    #a) print headers
     headers=response.headers
     print("Website Headers are: \n",headers)
 
+    #b) the sw that the server uses to response
     server= response.headers.get("Server")
     if server:
         print(f"The server is {server}")
     else:
         print("No server found")
 
-    cookies=response.cookies.get_dict()
-
+    #c) cookies and expiration
+    cookies=response.headers.get("Set-Cookie")
     print(cookies)
