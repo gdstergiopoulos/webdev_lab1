b0b36519f539042c1768fe0159f0ed31dd7c20ea
gdstergiopoulos <gdstergiopoulos@gmail.com>
2024-02-29 16:23:25 +0200

    attempt to prin only info needed (no regex)

--- lab1.py
+++ lab1.py
@@ -36,7 +36,32 @@ with requests.get(url) as response:  # το αντικείμενο response
         print("\nThe site uses the Cookies bellow: \n")
         print(cookies)
     
-   
+
+
+    cookiename_list=[]
+    x=response.cookies.get_dict().keys()
+    for i in x:
+        cookiename_list.append(i)
+        # print(i)
+    
+    #An atempt to print only information needed, without the use of regex. It will not work if the field after the name of the cookie is not the expiration date.
+    #the use of regex is not required (mentioned in lab)
+    expiration_date=[]
+    z=cookies.split(";")
+    for i in range(0,len(z)):
+        for j in cookiename_list:
+            if j in z[i]:
+                if("expires" in z[i+1]):
+                    expiration_date.append(z[i+1])
+                else:
+                    expiration_date.append("Unknown")
+            else:
+                continue
+
+    for i in range(len(cookiename_list)):
+        print("Cookie name: "+cookiename_list[i]+" and "+expiration_date[i])
+
+
         
 
     
